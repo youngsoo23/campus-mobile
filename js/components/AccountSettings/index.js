2289c119d79dead9295d3f9c24fbeba26092231d
Young Soo Oh <oys1234@hotmail.com>
2017-12-06 15:58:10 -0800

    get datas

--- js/components/AccountSettings/index.js
+++ js/components/AccountSettings/index.js
@@ -12,6 +12,9 @@ import {
   Right,
   Body
 } from "native-base";
+import api from "../../api.js";
+import {idnum} from "../login/index";
+import {username} from "../login/index";
 
 import styles from "./styles";
 
@@ -19,6 +22,26 @@ class AccountSettings extends Component {
   static navigationOptions = {
     header: null
   };
+    constructor(props){
+    super(props);
+    this.state = {
+      users: [],
+      userFirst: '',
+      userLast: ''
+    }
+  }
+  componentWillMount(){
+    api.getUsers().then((res) => {
+      this.setState({
+        users: res.users,
+        userFirst: res.users[idnum].FirstName,
+        userLast: res.users[idnum].LastName,
+        userDept: res.users[idnum].Department,
+        userYear: res.users[idnum].AcademicYear,
+
+      })
+    });
+  }
   render() {
     const { props: { name, index, list } } = this;
     return (
@@ -45,11 +68,11 @@ class AccountSettings extends Component {
         </Header>
 
         <Content padder>
-          <Text style={styles.text}>Name:</Text>
-          <Text style={styles.text}>Major:</Text>
+          <Text style={styles.text}>Name: {this.state.userFirst} {this.state.userLast}</Text>
+          <Text style={styles.text}>Major: {this.state.userDept}</Text>
           <Text style={styles.text}>E-mail:</Text> 
           <Text style={styles.text}>phone:</Text>
-          <Text style={styles.text}>Gender:</Text>  
+          <Text style={styles.text}>Gender: {this.state.userYear}</Text>  
         </Content>
 
       </Container>
